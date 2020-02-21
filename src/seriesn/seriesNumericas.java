c08c135f3744123a04083e7d206ff2fdb47637f3
Selene <Selene@192.168.1.71>
2020-02-20 17:27:59 -0700

    Version 1

---
+++ src/seriesn/seriesNumericas.java
@@ -0,0 +1,106 @@
+package seriesn;
+
+import java.util.Scanner;
+
+class seriesNumericas {
+
+    public static void main(String args[]) {
+        int opcion = 0;
+        int limite = 0;
+        System.out.println("\nBIENVENIDO A LAS SERIES NUMERICAS\n");
+        Scanner teclado = new Scanner(System.in);
+
+        do {
+            System.out.println("\nMENU\n1 SERIE FIBO\n2 SERIE PAVO\n3 TRIANGULO DE PASCAL\n4 "
+                    + "SALIR\n INGRESE UNO DE LOS NUMEROS MOSTRADOS Y PRESIONE ENTER\n");
+            //opcion = teclado.nextInt();
+            try {
+                opcion = teclado.nextInt();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("\nIngrese el numero que sera el limite\n");
+                        teclado = new Scanner(System.in);
+                        if (teclado.hasNextInt()) {
+                            limite = teclado.nextInt();
+                            fibo(limite);
+                        } else {
+                            System.out.println("ERROR");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("\nIngrese el numero que sera el limite\n");
+                        teclado = new Scanner(System.in);
+                        if (teclado.hasNextInt()) {
+                            limite = teclado.nextInt();
+                            pavo(limite);
+                        } else {
+                            System.out.println("ERROR");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("\nIngrese el numero que sera el limite\n");
+                        teclado = new Scanner(System.in);
+                        if (teclado.hasNextInt()) {
+                            limite = teclado.nextInt();
+                            pasqui(limite);
+                        } else {
+                            System.out.println("ERROR");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Salir del programa ...");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("\nOpcion ingresada no es valida\n");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("No es un caracter valido");
+                break;
+            }
+        } while (opcion != 4);
+    }
+
+    private static void fibo(int L) {
+        int n1 = 0, n2 = 1, n3, i, count = L;
+        System.out.print(n1 + " " + n2);
+        for (i = 2; i < count; ++i) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    private static void pavo(int L) {
+        int firstNumber = 1, secondNumber = 1, thirdNumber = 1;
+        int noOfTerms = L, nextNumber;
+        System.out.print(firstNumber + " " + secondNumber + " " + thirdNumber);
+        for (int i = 1; i <= noOfTerms - 3; i++) {
+            nextNumber = secondNumber + firstNumber;
+            System.out.print(" " + nextNumber);
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
+            thirdNumber = nextNumber;
+        }
+    }
+
+    private static void pasqui(int L) {
+        int rows = L;
+        if (rows <= 50) {
+            for (int i = 0; i < rows; i++) {
+                int number = 1;
+                System.out.format("%" + (rows - i) * 2 + "s", "");
+                for (int j = 0; j <= i; j++) {
+                    System.out.format("%4d", number);
+                    number = number * (i - j) / (j + 1);
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("\nERROR: LIMITE SUPERADO, MAYOR A 50\n");
+        }
+
+    }
+}
